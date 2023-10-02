b6f0ece895da313626c3ef3dfc12e8336ac8d6f4
nagamine <yochank003@gmail.com>
2023-10-01 19:33:51 -0500

    git blame enabled

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -2,17 +2,25 @@ local M = {
   "lewis6991/gitsigns.nvim",
   commit = "bdeba1cec3faddd89146690c10b9a87949c0ee66",
   event = "BufReadPre",
+  opts = {
+
+  }
 }
 
 function M.config()
   require('gitsigns').setup({
     signs = {
-   add = { text = "▎" },
-    change = { text = "▎" },
-    delete = { text = "" },
-    topdelete = { text = "" },
-    changedelete = { text = "▎" },
-    untracked = { text = "┋" },
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "┋" },
+    },
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts = {
+      virt_text_pos = 'overlay'
+      delay = 500,
     },
   })
 end
