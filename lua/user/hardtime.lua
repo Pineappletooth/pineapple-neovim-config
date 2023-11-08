c5f542534ce32e7e6394910dff541406203cd302
nagamine <yochank003@gmail.com>
2023-11-07 19:18:29 -0500

    tree climber skip comments

--- lua/user/hardtime.lua
+++ lua/user/hardtime.lua
@@ -3,7 +3,13 @@ return {
    commit = "8cc4dec29a177cb7c33a900ccf45b451684c30a0",
    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
    opts = {
-      max_count = 5,
-      disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil", "minifiles" },
+      max_count = 7,
+      disabled_filetypes = { "help","prompt","qf", "netrw", "NvimTree", "lazy", "mason", "oil", "minifiles", "trouble", "nvim-navbuddy" },
+      disabled_keys = {
+      ["<Up>"] = { "", "n" },
+      ["<Down>"] = { "", "n" },
+      ["<Left>"] = { "", "n" },
+      ["<Right>"] = { "", "n" },
+   }
    },
 }
--- lua/user/tree-climber.lua
+++ lua/user/tree-climber.lua
@@ -4,10 +4,10 @@ return {
    event = {"BufEnter" },
    config = function ()
       local keyopts = { noremap = true, silent = true }
-      vim.keymap.set({'n', 'v', 'o'}, '°', require('tree-climber').goto_parent, keyopts)
-      vim.keymap.set({'n', 'v', 'o'}, '|', require('tree-climber').goto_child, keyopts)
-      vim.keymap.set({'n', 'v', 'o'}, '<tab>', require('tree-climber').goto_next, keyopts)
-      vim.keymap.set({'n', 'v', 'o'}, '<S-TAB>', require('tree-climber').goto_prev, keyopts)
+      vim.keymap.set({'n', 'v', 'o'}, '°', function() require('tree-climber').goto_parent({skip_comments = true}) end, keyopts)
+      vim.keymap.set({'n', 'v', 'o'}, '|', function() require('tree-climber').goto_child({skip_comments = true}) end, keyopts)
+      vim.keymap.set({'n', 'v', 'o'}, '<tab>', function() require('tree-climber').goto_next({skip_comments = true}) end, keyopts)
+      vim.keymap.set({'n', 'v', 'o'}, '<S-TAB>', function() require('tree-climber').goto_prev({skip_comments = true}) end, keyopts)
       vim.keymap.set({'v', 'o'}, 'in', require('tree-climber').select_node, keyopts)
       vim.keymap.set('n', '<c-k>', require('tree-climber').swap_prev, keyopts)
       vim.keymap.set('n', '<c-j>', require('tree-climber').swap_next, keyopts)
