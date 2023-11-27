c34aba31e86b11035287a315a3e4848a79bd3ae7
Nagamine <jimmy.kochi@unmsm.edu.pe>
2023-11-26 19:48:12 -0500

    add git conflict

--- lua/plugins/conflictmarker.lua
+++ lua/plugins/conflictmarker.lua
@@ -1,1 +1,5 @@
-return {'akinsho/git-conflict.nvim', version = "*", lazy = false, config = true}
+return {
+  'rhysd/conflict-marker.vim',
+  commit="11a4d42244755505b66b15cd4496a150432eb5e3",
+  event={'BufEnter'},
+}
