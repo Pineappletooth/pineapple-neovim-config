40e75563dc67a90973ce3f5db0febc3e91e492f4
nagamine <yochank003@gmail.com>
2024-07-14 20:22:39 -0500

    fix dynamic content

--- lua/plugins/staline.lua
+++ lua/plugins/staline.lua
@@ -2,11 +2,14 @@ return {
   'tamton-aquib/staline.nvim',
   lazy=false,
   config = function ()
+    local function getSpaces()
+        return 'Spaces: '.. vim.api.nvim_get_option_value("shiftwidth",{})
+    end
     require('staline').setup({
       sections = {
         left = { '- ', '-mode', 'left_sep_double', 'branch', '','cwd', '','lsp' },
         mid = {'file_name'},
-        right = { 'Spaces: '.. vim.api.nvim_get_option_value("shiftwidth",{}),'  ',vim.bo.fileencoding:upper(),'right_sep_double', '-line_column' },
+        right = { {'Staline',getSpaces},'  ',vim.bo.fileencoding:upper(),'right_sep_double', '-line_column' },
       },
       defaults = {
         line_column     = "[%l:%c] %p%%"
