9c32f44866eaf596b433f5767c003c1f42a5f795
nagamine <yochank003@gmail.com>
2024-07-14 20:02:39 -0500

    eye candy

--- lua/plugins/staline.lua
+++ lua/plugins/staline.lua
@@ -6,7 +6,7 @@ return {
       sections = {
         left = { '- ', '-mode', 'left_sep_double', 'branch', '','cwd', '','lsp' },
         mid = {'file_name'},
-        right = { 'lsp_name', '  ' ,'Spaces: '.. vim.api.nvim_get_option_value("shiftwidth",{}),'  ',vim.bo.fileencoding:upper(),'right_sep_double', '-line_column' },
+        right = { 'Spaces: '.. vim.api.nvim_get_option_value("shiftwidth",{}),'  ',vim.bo.fileencoding:upper(),'right_sep_double', '-line_column' },
       },
       defaults = {
         line_column     = "[%l:%c] %p%%"
